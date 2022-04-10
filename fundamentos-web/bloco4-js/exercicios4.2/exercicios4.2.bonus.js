32f81ae884e75e9c4c60d5fe266268bbb55c2552
Raissa V <rai.celos20@gmail.com>
2022-04-10 12:29:35 -0300

    Realização da parte bônus do bloco 4.2.

--- fundamentos-web/bloco4-js/exercicios4.2/exercicios4.2.bonus.js
+++ fundamentos-web/bloco4-js/exercicios4.2/exercicios4.2.bonus.js
@@ -14,7 +14,40 @@ for(let index=1; index<numbers.length;index+=1){
             numbers[index2] = numbers[index];
             numbers[index] = aux;
         }
-
 }
 
 console.log(numbers);
+
+//Ordenação do texto em ordem decrescente
+
+for(let index3=1; index3<numbers.length; index3+=1){
+    for(let index4=0; index4<index3; index4+=1)
+        if(numbers[index3]>numbers[index4]){
+            let aux2 = numbers[index4];
+            numbers[index4] = numbers[index3];
+            numbers[index3] = aux2;  
+        }
+}
+console.log(numbers);
+
+let numbers12 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbersMultip = [];
+let tamanho = numbers12.length;
+
+for(let index5=1; index5<=tamanho; index5+=1){
+        if(index5<numbers12.length){
+            let aux3 = numbers12[index5]*numbers12[index5-1];
+            numbersMultip.push(aux3);
+        }
+        else{
+            let aux3 = numbers12[index5-1]*2;
+            numbersMultip.push(aux3);
+        }
+}
+console.log(numbersMultip);
+
+//resposta = 45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+
+/*for(let i=0; i<15; i+=1){
+    console.log(i);
+}*/
